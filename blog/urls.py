4b783ce46c58a4d0f591a8873fb8c2bd4d0c6423
rileylee <liyizhou@me.com>
2018-01-24 14:18:40 +0800

    change blog/view.py post_list to Class-based-view

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),  # blog/页面映射到post_list视图
+    # url(r'^$', views.post_list, name='post_list'),  # blog/页面映射到post_list视图
+    url(r'^$', views.PostListView.as_view(), name='post_list',),  # 基于类的视图 类中有方法as_view()将其转化为view
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',  # url由一个正则+一个view+一个全项目范围内的name构成
         views.post_detail,
         name='post_detail', ),  # blog/日期/slug/页面映射到post_detail视图
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,23 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):  # request作为唯一参数，所有视图都需要
-    object_list = Post.published.all()  # 调用自制管理器 检索期望的Post
-    paginator = Paginator(object_list, 3)  # 设置每页有几个object
-    page = request.GET.get('page')  # 获取参数指明页数
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)  # 若page不是int型 则转到第一页
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)  # 若page超出范围 则转到最后一页
-    return render(request,
-                  'blog/post/list.html',
-                  {'page': page,
-                   'posts': posts}, )
+# def post_list(request):  # request作为唯一参数，所有视图都需要
+#     object_list = Post.published.all()  # 调用自制管理器 检索期望的Post
+#     paginator = Paginator(object_list, 3)  # 设置每页有几个object
+#     page = request.GET.get('page')  # 获取参数指明页数
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)  # 若page不是int型 则转到第一页
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)  # 若page超出范围 则转到最后一页
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'page': page,
+#                    'posts': posts}, )
 
 
 def post_detail(request, year, month, day, post):
@@ -28,3 +29,10 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post}, )  # 通过render渲染模版template
+
+
+class PostListView(ListView):  # 基于类的视图
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
